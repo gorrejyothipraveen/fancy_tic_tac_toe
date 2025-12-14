f5f54d41e49da93928010b1894b1383bea57fcca
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2025-12-14 23:06:09 +0530

    implemented code for to check whether any rows or cols or diagonals equal

--- src/tic_tac_toe.js
+++ src/tic_tac_toe.js
@@ -3,7 +3,27 @@
 export const areRowsEqual = (board) =>
   board.some((row) => row.every((x) => row[0] === x && x !== " "));
 
-// const areColEquals = (board) => board.some((_, i) => board.every((x, j) => board[i]))
+export const areColEquals = (board) => {
+  for (let i = 0; i < board.length; i++) {
+    let flag = false;
+    for (let j = 0; j < board.length; j++) {
+      if (board[j][i] !== board[0][i] || board[j][i] === " ") {
+        flag = true;
+      }
+    }
+    if (!flag) {
+      return true;
+    }
+  }
+};
+
+export const areDiagonalEqualsFromLToR = (board) =>
+  board.every((row, i) => row[i] !== " " && row[i] === board[0][0]);
+
+export const areDiagonalEqualsFromRToL = (board) => {
+  return areDiagonalEqualsFromLToR(board.map(x => x.toReversed()));
+}
+
 
 // const winningLocations = (gameState) => {
 //   if (areRowsEqual(gameState.board)) {
--- test/tic_tac_toe_test.js
+++ test/tic_tac_toe_test.js
@@ -1,11 +1,110 @@
-import {assertEquals} from '@std/assert';
-import { areRowsEqual } from "../src/tic_tac_toe.js";
+import { assertEquals } from "@std/assert";
+import { areColEquals, areDiagonalEqualsFromLToR, areDiagonalEqualsFromRToL, areRowsEqual } from "../src/tic_tac_toe.js";
 
-Deno.test('is any row equals :', () => {
+Deno.test("is any row equals :", () => {
   const board = [
-    [' ', ' ', ' '], 
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-  ]
-  assertEquals(areRowsEqual(board), false)
-})
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+  assertEquals(areRowsEqual(board), false);
+});
+
+Deno.test("first row is equal :", () => {
+  const board = [
+    ["O", "O", "O"],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+  assertEquals(areRowsEqual(board), true);
+});
+
+Deno.test("second row is equal:", () => {
+  const board = [
+    [" ", " ", " "],
+    ["O", "O", "O"],
+    [" ", " ", " "],
+  ];
+  assertEquals(areRowsEqual(board), true);
+});
+
+Deno.test("no rows are equal:", () => {
+  const board = [
+    [" ", " ", " "],
+    ["O", "X", "O"],
+    [" ", " ", " "],
+  ];
+  assertEquals(areRowsEqual(board), false);
+});
+
+Deno.test("1st column elements are equal:", () => {
+  const board = [
+    ["O", " ", " "],
+    ["O", "X", "O"],
+    ["O", " ", " "],
+  ];
+  assertEquals(areColEquals(board), true);
+});
+
+Deno.test("2st column elements are equal:", () => {
+  const board = [
+    ["O", "X", " "],
+    ["O", "X", "O"],
+    ["X", "X", " "],
+  ];
+  assertEquals(areColEquals(board), true);
+});
+
+Deno.test("diagonal elements are not equal: from L to R", () => {
+  const board = [
+    ["X", "X", " "],
+    ["O", " ", "O"],
+    ["X", "X", "X"],
+  ];
+  assertEquals(areDiagonalEqualsFromLToR(board), false);
+});
+
+Deno.test("diagonal elements are not equal: from L to R", () => {
+  const board = [
+    ["X", "X", " "],
+    ["O", " ", "O"],
+    ["X", "X", "X"],
+  ];
+  assertEquals(areDiagonalEqualsFromLToR(board), false);
+});
+
+Deno.test("diagonal elements are not equal: from L to R", () => {
+  const board = [
+    ["O", "X", " "],
+    ["O", "O", "O"],
+    ["X", "X", "X"],
+  ];
+  assertEquals(areDiagonalEqualsFromLToR(board), false);
+});
+
+Deno.test("diagonal elements are equal: from L to R", () => {
+  const board = [
+    ["O", "X", " "],
+    ["O", "O", "O"],
+    ["X", "X", "O"],
+  ];
+  assertEquals(areDiagonalEqualsFromLToR(board), true);
+});
+
+Deno.test("diagonal elements are not equal: from R to L", () => {
+  const board = [
+    ["O", "X", " "],
+    ["O", "O", "O"],
+    ["X", "X", "O"],
+  ];
+  assertEquals(areDiagonalEqualsFromRToL(board), false);
+});
+
+Deno.test("diagonal elements are not equal: from R to L", () => {
+  const board = [
+    ["O", "X", "O"],
+    ["O", "O", "O"],
+    ["O", "X", "O"],
+  ];
+  assertEquals(areDiagonalEqualsFromRToL(board), true);
+});
